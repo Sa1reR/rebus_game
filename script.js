ec5d70534c917bc70c802114d0a7f5334cd23b46
valeri.stakhova <valeri.stakhova@gmail.com>
2022-11-08 12:40:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+var button = document.getElementById("button");
+var task1 = document.getElementById("task1");
+var res = document.getElementById("result");
+var a = 0
+button.addEventListener("click", function (){
+    if (task1.value == "github") {
+        a++;
+        console.log(a);
+        button.setAttribute("disabled", true);
+    }
+    else {
+    alert("неправильно");
+    }
+})
